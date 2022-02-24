cb9c58f746e72f3945556830024c31e221a38d49
lizzz <20071724@mail.wit.ie>
2022-02-24 16:24:15 +0000

    All the functions have been placed and can be called in main() which will run the menu and display it to the user

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,65 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.System.exit
+import java.util.*
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+val scanner = Scanner(System.`in`)
+
+fun main(args: Array<String>) {
+    runMenu()
+}
+
+
+fun mainMenu() : Int{
+    println("")
+    println("---------------------")
+    println("NOTE KEEPER APP")
+    println("--------------------")
+    println("NOTE MENU")
+    println(" 1) Add a Note")
+    println(" 2) List all Notes")
+    println(" 3) Update a Note")
+    println(" 4) Delete a Node")
+    println("--------------------")
+    println(" 0) Exit")
+    println("--------------------")
+    println("==>> ")
+    return scanner.nextInt()
+
+}
+
+fun runMenu(){
+    do{
+        val option = mainMenu()
+        when(option){
+            1 -> addNote()
+            2 -> listNotes()
+            3 -> updateNotes()
+            4 -> deleteNote()
+            0 -> exitApp()
+            else -> System.out.println("Invalid option entered: " + option)
+        }
+
+    }
+    while(true)
+
+}
+
+fun addNote(){
+    println("You choose Add Note")
+}
+
+fun listNotes(){
+    println("You choose Add Note")
+}
+
+fun updateNotes(){
+    println("You choose Add Note")
+}
+
+fun deleteNote(){
+    println("You choose Add Note")
+}
+
+fun exitApp() {
+    println("Exiting...Goodbye")
+    exit(0)
 }
